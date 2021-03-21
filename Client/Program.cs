eb579a7d13e7c47fa90e3adfae3095b3c38a6c95
Akhmadov Andarbek <ba@trinitydigital.ru>
2021-03-21 22:14:35 +0300

    Initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        private const int serverPort = 2365;
+        private static TcpClient client;
+        private static string fullmessage;
+        private static bool isRunning = true;
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Tcp User start");
+            var gameCount = 0;
+            var maxStep = 0;
+            var count = "";
+            var step = "";
+            do
+            {
+                Console.Write("Enter game count: ");
+                count = Console.ReadLine();
+                Console.Write("Enter max step: ");
+                step = Console.ReadLine();
+                if (int.TryParse(count, out gameCount) && int.TryParse(step, out maxStep) && gameCount > maxStep)
+                {
+                    fullmessage = count + ',' + step;
+                }
+                else
+                {
+                    Console.WriteLine("Input correct numbers");
+                }
+            } while (!int.TryParse(count, out gameCount) || !int.TryParse(step, out maxStep) || gameCount <= maxStep);
+
+            client = new TcpClient();
+            await client.ConnectAsync(IPAddress.Loopback, serverPort);
+            NetworkStream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream);
+            await writer.WriteLineAsync(fullmessage);
+            await writer.FlushAsync();
+            string answer = await reader.ReadLineAsync();
+            Console.WriteLine(answer);
+            while (isRunning)
+            {
+                var nowStep = "";
+                var stepClient = 0;
+                do
+                {
+                    Console.Write("Enter your step: ");
+                    nowStep = Console.ReadLine();
+
+                    if (int.TryParse(nowStep, out stepClient) && (stepClient > 0 && stepClient <= maxStep))
+                    {
+                        Console.WriteLine("OK");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please, input correct step");
+                    }
+
+                } while (!int.TryParse(nowStep, out stepClient) || stepClient <= 0 || stepClient > maxStep);
+
+                
+                await writer.WriteLineAsync(nowStep);
+                await writer.FlushAsync();
+                answer = await reader.ReadLineAsync();
+                if (answer == "Server is win!" || answer == "Client is win!")
+                {
+                    isRunning = false;
+                }
+                Console.WriteLine(answer);
+            }
+            client.Close();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+//using Blog;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+//using Microsoft.EntityFrameworkCore.Storage;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace Server
+{
+    class Program
+    {
+        private static TcpListener listener;
+        private const int serverPort = 2365;
+        private static bool run;
+        private static int step;
+        private static int gameCount;
+        private static bool isGaming = true;
+        private static TcpClient client;
+        private static NetworkStream stream;
+        private static StreamReader reader;
+        private static StreamWriter writer;
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Server start");
+            listener = new TcpListener(IPAddress.Any, serverPort);
+            run = true;
+            await Listen();
+        }
+
+        private static async Task Listen()
+        {
+            listener.Start();
+            client = await listener.AcceptTcpClientAsync();
+            stream = client.GetStream();
+            reader = new StreamReader(stream);
+            writer = new StreamWriter(stream);
+            do
+            {
+                string fullmessage = await reader.ReadLineAsync();
+                if (fullmessage != null)
+                {
+                    string[] listMessage = fullmessage.Split(",");
+                    if (int.TryParse(listMessage[0], out gameCount) && int.TryParse(listMessage[1], out step))
+                    {
+                        break;
+                    }
+                }
+            } while (gameCount <= step);
+            await Game();
+        }
+
+        private static async Task Game()
+        {
+            while (isGaming)
+            {
+                if (gameCount != 0)
+                {
+                    var serverRemoveCount = CheckStatus();
+                    gameCount -= serverRemoveCount;
+                    if (gameCount == 0)
+                    {
+                        writer = new StreamWriter(stream);
+                        await writer.WriteLineAsync("Server is win!");
+                        await writer.FlushAsync();
+                        isGaming = false;
+                    }
+                    var str = "Server choice is " + serverRemoveCount + " Left count " + gameCount; 
+                    await writer.WriteLineAsync(str);
+                    await writer.FlushAsync();
+                    var clientResponce = await reader.ReadLineAsync();
+                    int clientStep;
+                    if (int.TryParse(clientResponce, out clientStep))
+                    {
+                        gameCount -= clientStep;
+                        if (gameCount == 0)
+                        {
+                            writer = new StreamWriter(stream);
+                            await writer.WriteLineAsync("Client is win!");
+                            await writer.FlushAsync();
+                            isGaming = false;
+                        }
+                    }
+                }
+            }
+            client.Close();
+        }
+        private static int CheckStatus()
+        {
+            var checkStep = step + 1;
+            Random rnd = new Random();
+            var howTake = 0;
+            var checkLeft = gameCount / checkStep;
+            var needForWin = gameCount - checkLeft * checkStep;
+
+            if (needForWin > 0 && needForWin <= step)
+            {
+                howTake = (int) needForWin;
+            }
+            else
+            {
+                howTake = rnd.Next(1, (int) step);
+            }
+            return howTake;
+        }
+    }
+}
